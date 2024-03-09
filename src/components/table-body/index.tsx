3b1c565a95621257f1a1b4ad8c180a3138d94f2d
yukie.terasaki <yukie_terasaki@betterbound.jp>
2024-03-09 14:52:45 +0900

    Add @dnd-kit dependencies and test

--- src/components/table-body/index.tsx
+++ src/components/table-body/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import classNames from 'classnames'
 import Context from '../../context'
@@ -6,11 +6,123 @@ import { TOP_PADDING } from '../../constants'
 import RowToggler from './RowToggler'
 import './index.less'
 
-const TableRows = () => {
+import {
+  DndContext, 
+  KeyboardSensor,
+  PointerSensor,
+  closestCenter,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import {
+  SortableContext,
+  arrayMove,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+
+const SortableTableRows = ({bar, rowIndex, start, isLastChild, prefixClsTableBody, rowHeight, onRow,columns,columnsWidth,tableIndent,expandIcon, setRowCollapse, onExpand, prefixCls}) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({id: bar.key})
+
+  return (
+    <div
+      role='none'
+      className={`${prefixClsTableBody}-row ${bar.record.className}`}
+      style={{
+        height: rowHeight,
+        top: (rowIndex + start) * rowHeight + TOP_PADDING,
+      }}
+      onClick={() => {
+        onRow?.onClick(bar.record)
+      }}
+      ref={setNodeRef}{...attributes} {...listeners}
+    >
+      {columns.map((column, index) => (
+        <div
+          key={column.name}
+          className={`${prefixClsTableBody}-cell`}
+          style={{
+            width: columnsWidth[index],
+            minWidth: column.minWidth,
+            maxWidth: column.maxWidth,
+            textAlign: column.align ? column.align : 'left',
+            paddingLeft: index === 0 && tableIndent * (bar._depth + 1) + 10,
+            ...column.style,
+          }}
+        >
+          {index === 0 &&
+            // eslint-disable-next-line unicorn/no-new-array
+            new Array(bar._depth).fill(0).map((_, i) => (
+              <div
+                // eslint-disable-next-line react/no-array-index-key
+                key={i}
+                className={classNames(`${prefixClsTableBody}-row-indentation`, {
+                  [`${prefixClsTableBody}-row-indentation-hidden`]: isLastChild && i === bar._depth - 2,
+                  [`${prefixClsTableBody}-row-indentation-both`]: i === bar._depth - 1,
+                })}
+                style={{
+                  top: -(rowHeight / 2) + 1,
+                  left: tableIndent * i + 15,
+                  width: tableIndent * 1.5 + 5,
+                }}
+              />
+            ))}
+          {index === 0 && bar._childrenCount > 0 && (
+            <div
+              style={{
+                position: 'absolute',
+                left: tableIndent * bar._depth + 15,
+                background: 'white',
+                zIndex: 9,
+                transform: 'translateX(-52%)',
+                padding: 1,
+              }}
+            >
+              {expandIcon ? (
+                expandIcon({
+                  level: bar._depth,
+                  collapsed: bar._collapsed,
+                  onClick: event => {
+                    event.stopPropagation()
+                    if (onExpand) onExpand(bar.task.record, !bar._collapsed)
+                    setRowCollapse(bar.task, !bar._collapsed)
+                  },
+                })
+              ) : (
+                <RowToggler
+                  prefixCls={prefixCls}
+                  level={bar._depth}
+                  collapsed={bar._collapsed}
+                  onClick={event => {
+                    event.stopPropagation()
+                    if (onExpand) onExpand(bar.task.record, !bar._collapsed)
+                    setRowCollapse(bar.task, !bar._collapsed)
+                  }}
+                />
+              )}
+            </div>
+          )}
+          <span className={`${prefixClsTableBody}-ellipsis`}>
+            {column.render ? column.render(bar.record) : bar.record[column.name]}
+          </span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const TableRows = ({barList}) => {
   const { store, onRow, tableIndent, expandIcon, prefixCls, onExpand } = useContext(Context)
   const { columns, rowHeight } = store
   const columnsWidth = store.getColumnsWidth
-  const barList = store.getBarList
+  // const barList = store.getBarList
 
   const { count, start } = store.getVisibleRows
   const prefixClsTableBody = `${prefixCls}-table-body`
@@ -28,6 +140,7 @@ const TableRows = () => {
       </div>
     )
   }
+  console.log("barList",barList.slice(start, start + count))
   return (
     <>
       {barList.slice(start, start + count).map((bar, rowIndex) => {
@@ -38,91 +151,23 @@ const TableRows = () => {
         if (parentItem?.children && parentItem.children[parentItem.children.length - 1] === bar._parent)
           isLastChild = true
 
-        return (
-          <div
-            key={bar.key}
-            role='none'
-            className={`${prefixClsTableBody}-row ${bar.record.className}`}
-            style={{
-              height: rowHeight,
-              top: (rowIndex + start) * rowHeight + TOP_PADDING,
-            }}
-            onClick={() => {
-              onRow?.onClick(bar.record)
-            }}
-          >
-            {columns.map((column, index) => (
-              <div
-                key={column.name}
-                className={`${prefixClsTableBody}-cell`}
-                style={{
-                  width: columnsWidth[index],
-                  minWidth: column.minWidth,
-                  maxWidth: column.maxWidth,
-                  textAlign: column.align ? column.align : 'left',
-                  paddingLeft: index === 0 && tableIndent * (bar._depth + 1) + 10,
-                  ...column.style,
-                }}
-              >
-                {index === 0 &&
-                  // eslint-disable-next-line unicorn/no-new-array
-                  new Array(bar._depth).fill(0).map((_, i) => (
-                    <div
-                      // eslint-disable-next-line react/no-array-index-key
-                      key={i}
-                      className={classNames(`${prefixClsTableBody}-row-indentation`, {
-                        [`${prefixClsTableBody}-row-indentation-hidden`]: isLastChild && i === bar._depth - 2,
-                        [`${prefixClsTableBody}-row-indentation-both`]: i === bar._depth - 1,
-                      })}
-                      style={{
-                        top: -(rowHeight / 2) + 1,
-                        left: tableIndent * i + 15,
-                        width: tableIndent * 1.5 + 5,
-                      }}
-                    />
-                  ))}
-                {index === 0 && bar._childrenCount > 0 && (
-                  <div
-                    style={{
-                      position: 'absolute',
-                      left: tableIndent * bar._depth + 15,
-                      background: 'white',
-                      zIndex: 9,
-                      transform: 'translateX(-52%)',
-                      padding: 1,
-                    }}
-                  >
-                    {expandIcon ? (
-                      expandIcon({
-                        level: bar._depth,
-                        collapsed: bar._collapsed,
-                        onClick: event => {
-                          event.stopPropagation()
-                          if (onExpand) onExpand(bar.task.record, !bar._collapsed)
-                          store.setRowCollapse(bar.task, !bar._collapsed)
-                        },
-                      })
-                    ) : (
-                      <RowToggler
-                        prefixCls={prefixCls}
-                        level={bar._depth}
-                        collapsed={bar._collapsed}
-                        onClick={event => {
-                          event.stopPropagation()
-                          if (onExpand) onExpand(bar.task.record, !bar._collapsed)
-                          store.setRowCollapse(bar.task, !bar._collapsed)
-                        }}
-                      />
-                    )}
-                  </div>
-                )}
-                <span className={`${prefixClsTableBody}-ellipsis`}>
-                  {column.render ? column.render(bar.record) : bar.record[column.name]}
-                </span>
-              </div>
-            ))}
-          </div>
-        )
+          return <SortableTableRows 
+          bar={bar} 
+          rowIndex={rowIndex}
+          start={start}
+          isLastChild={isLastChild}
+           prefixClsTableBody={prefixClsTableBody}
+           rowHeight={rowHeight}
+           onRow={onRow}
+          columns={columns}
+          columnsWidth={columnsWidth}
+          tableIndent={tableIndent}
+          expandIcon={expandIcon}
+           setRowCollapse={store.setRowCollapse}
+           onExpand={onExpand}
+           prefixCls={prefixCls}
+          />
+
       })}
     </>
   )
@@ -158,6 +203,8 @@ const ObserverTableBorders = observer(TableBorders)
 
 const TableBody: React.FC = () => {
   const { store, prefixCls } = useContext(Context)
+
+  const [barList, setBarList] = useState(store.getBarList)
   const handleMouseMove = useCallback(
     (event: React.MouseEvent<HTMLDivElement>) => {
       event.persist()
@@ -169,19 +216,48 @@ const TableBody: React.FC = () => {
     store.handleMouseLeave()
   }, [store])
   const prefixClsTableBody = `${prefixCls}-table-body`
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  )
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+
+    const oldIndex = barList.findIndex((item) => item.key === active.id)
+    const newIndex = barList.findIndex((item) => item.key === over.id)
+
+    if (active.id !== over.id) {
+      console.log(active,over)
+      setBarList((items) => 
+      arrayMove(items, oldIndex, newIndex))
+    }
+  }
+
+  useEffect(() => {
+    setBarList(store.getBarList)
+  }, [store.getBarList])
+
   return (
-    <div
-      className={prefixClsTableBody}
-      style={{
-        width: store.tableWidth,
-        height: store.bodyScrollHeight,
-      }}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-    >
-      <ObserverTableBorders />
-      <ObserverTableRows />
-    </div>
+    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <div
+        className={prefixClsTableBody}
+        style={{
+          width: store.tableWidth,
+          height: store.bodyScrollHeight,
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+      >
+        <ObserverTableBorders />
+        <SortableContext items={barList.map(item => item.key)} strategy={verticalListSortingStrategy}>
+          <ObserverTableRows barList={barList} />
+        </SortableContext>
+      </div>
+    </DndContext>
   )
 }
 export default observer(TableBody)
--- website/demo/basic.en-US.tsx
+++ website/demo/basic.en-US.tsx
@@ -2,6 +2,21 @@ import dayjs from 'dayjs'
 import RcGantt, { enUS } from 'rc-gantt'
 import React, { useState } from 'react'
 
+import {
+  DndContext, 
+  KeyboardSensor,
+  PointerSensor,
+  closestCenter,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import {
+  SortableContext,
+  arrayMove,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
 interface Data {
   id: number
   name: string
@@ -77,76 +92,158 @@ function createData(len: number) {
   return result
 }
 
+const SortableItem = (props) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({id: props.id})
+  
+  return (
+    <div ref={setNodeRef}{...attributes} {...listeners}>
+  アイテム{props.id}
+    </div>
+  )
+}
+
 const App = () => {
-  const [data, setData] = useState(createData(10))
+  const [data, setData] = useState(createData(3))
   console.log('data', data)
   const handleClick = (e) => {
     console.log(e, e.currentTarget.value, e.currentTarget.innerText)
   }
+
+  const [items, setItems] = useState([1, 2, 3,4,5])
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  )
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+
+    if (active.id !== over.id) {
+
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id)
+        const newIndex = items.indexOf(over.id)
+        return arrayMove(items, oldIndex, newIndex)
+      })
+    }
+  }
+
+
   return (
-    <div style={{ width: '100%', height: 500 }}>
-      <RcGantt<Data>
-        data={data}
-        columns={[
-          {
-            name: 'name',
-            label: 'Custom Title',
-            width: 52,
-          },
-          {
-            name: 'name',
-            label: 'Custom Title',
-          },
-          {
-            name: 'name',
-            label: 'Custom Title',
-            width: 40,
-          },
-          {
-            name: 'name',
-            label: 'Custom Title',
-            width: 55,
-          },
-          {
-            name: 'name',
-            label: 'Custom Title',
-            width: 100,
-          },
-          {
-            name: 'name',
-            label: 'Custom Title',
-            width: 85,
-          },
-        ]}
-        // onExpand={}
-        unit='week_in_month'
-        showUnitSwitch={false}
-        locale={enUS}
-        onUpdate={async (row, startDate, endDate) => {
-          console.log('update', row, startDate, endDate)
-          setData(prev => {
-            const newList = [...prev]
-            const index = newList.findIndex(val => val.id === row.id)
-            newList[index] = {
-              ...row,
-              startDate: dayjs(startDate).format('YYYY-MM-DD'),
-              endDate: dayjs(endDate).format('YYYY-MM-DD'),
-            }
-            return newList
-          })
-          return true
-        }}
-        onTimeAxisClick={handleClick}
-        renderDaysText={() => ''}
-        showChangeBarSize={false}
-        canMoveBar={false}
-        timeAxisMinorStyle={{ color: '#006ec8' }}
-        tableSize={{
-          minWidth: 684,
-          maxWidth: 1080,
-        }}
-      />
-    </div>
+    <>
+      <div style={{ width: '100%', height: 500 }}>
+        <RcGantt<Data>
+          data={data}
+          columns={[
+            {
+              name: 'hotTopic',
+              label: 'HT',
+              width: 52,
+              style: {
+                padding: 0,
+              },
+            },
+            {
+              name: 'highPriority',
+              label: 'HP',
+              width: 52,
+              style: {
+                padding: 0,
+              },
+            },
+            {
+              name: 'title',
+              label: 'OutputGanttToggleButtons',
+              render: (record) =>{console.log(record.record)
+               
+                return (
+                  <p>{record.name}</p>
+                )},
+              style: {
+                padding: 0,
+              },
+            },
+            {
+              name: 'bookmark',
+              label: 'B',
+              width: 55,
+              style: {
+                padding: 0,
+              },
+            },
+            {
+              name: 'assignees',
+              label: '',
+              width: 100,
+              style: {
+                padding: 0,
+              },
+            },
+            {
+              name: 'status',
+              label: '',
+              width: 85,
+              style: {
+                padding: 0,
+              },
+            },
+            {
+              name: 'period',
+              label: '',
+              width: 85,
+              style: {
+                padding: 0,
+              },
+            },
+          ]}
+          // onExpand={}
+          unit='week_in_month'
+          showUnitSwitch={false}
+          locale={enUS}
+          onUpdate={async (row, startDate, endDate) => {
+            // console.log('update', row, startDate, endDate)
+            setData(prev => {
+              const newList = [...prev]
+              const index = newList.findIndex(val => val.id === row.id)
+              newList[index] = {
+                ...row,
+                startDate: dayjs(startDate).format('YYYY-MM-DD'),
+                endDate: dayjs(endDate).format('YYYY-MM-DD'),
+              }
+              return newList
+            })
+            return true
+          }}
+          onTimeAxisClick={handleClick}
+          renderDaysText={() => ''}
+          showChangeBarSize={false}
+          canMoveBar={false}
+          timeAxisMinorStyle={{ color: '#006ec8' }}
+          tableSize={{
+            minWidth: 684,
+            maxWidth: 1080,
+          }}
+        />
+      </div>
+     <p>↓確認用</p>
+      <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+          {items.map((id) => (
+            <SortableItem key={id} id={id} />
+          ))}
+        </SortableContext>
+      </DndContext>
+    </>
+
   )
 }
 
